df6ce9c9f365a978643f6f64b6258b54f487b948
Rafael <rafael_unis@hotmail.com>
2024-06-22 17:24:33 +0200

    First commit, created project whith spring initializr

---
+++ src/main/java/com/rafael/test_feefo/TestFeefoApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.test_feefo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFeefoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFeefoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/test_feefo/TestFeefoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.test_feefo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFeefoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
